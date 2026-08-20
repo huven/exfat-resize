1f3ddd52ecf983885192797c691a72ff69880a1d
huven <richard@huveneers.com>
2026-08-20 12:04:52 +0200

    Simplify geometry arithmetic (#42)
    Replace unreachable checked add and multiply paths with widened arithmetic.
    Retain checked ceiling division and FAT allocation narrowing validation.

--- lib/checked_math.c
+++ lib/checked_math.c
@@ -4,28 +4,6 @@
 
 #include "checked_math.h"
 
-enum exfat_resize_error exfat_resize_checked_add_u64(
-    uint64_t left, uint64_t right, uint64_t *result)
-{
-	if (result == NULL)
-		return EXFAT_RESIZE_INVALID_ARGUMENT;
-	if (right > UINT64_MAX - left)
-		return EXFAT_RESIZE_ARITHMETIC_OVERFLOW;
-	*result = left + right;
-	return EXFAT_RESIZE_SUCCESS;
-}
-
-enum exfat_resize_error exfat_resize_checked_multiply_u64(
-    uint64_t left, uint64_t right, uint64_t *result)
-{
-	if (result == NULL)
-		return EXFAT_RESIZE_INVALID_ARGUMENT;
-	if (left != 0 && right > UINT64_MAX / left)
-		return EXFAT_RESIZE_ARITHMETIC_OVERFLOW;
-	*result = left * right;
-	return EXFAT_RESIZE_SUCCESS;
-}
-
 enum exfat_resize_error exfat_resize_checked_ceil_divide_u64(
     uint64_t dividend, uint64_t divisor, uint64_t *result)
 {
